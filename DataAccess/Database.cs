6647b6e4528255eb4efd9502aafec37331f3ad81
Quoc Toan <Mail.Toan95@gmail.com>
2017-06-18 10:54:52 +0700

    Khởi tại ứng dụng

---
+++ DataAccess/Database.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DataAccess
+{
+    class Database
+    {
+        MySqlConnection con;
+        MySqlDataAdapter da;
+        DataSet ds;
+        public Database()
+        {
+            string sql = "Server=db4free.net;Port=3307;database=qlhd;User Id=nghiatran0595;Password=nghiatran0595;charset=utf8";
+            con = new MySqlConnection(sql);
+        }
+
+        public bool Execute(string sql)
+        {
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public DataTable LoadData(string sql)
+        {
+            ds = new DataSet();
+            da = new MySqlDataAdapter(sql, con);
+            da.Fill(ds);
+            return ds.Tables[0];
+        }
+    }
+}
