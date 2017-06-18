f757bb55cb634d3a098714fe935e4c55a55eeaf5
ThanhTan <phanthanhtan141295@gmail.com>
2017-06-18 12:10:07 +0700

    Dang Nhap

---
+++ DataAccess/DangNhap_DAL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using DataTransferObject;
+
+namespace DataAccess
+{
+    public class DangNhap_DAL
+    {
+        private readonly Database data = new Database();
+        public NhanVien_Object DangNhap(string taiKhoan, string matKhau)
+        {
+            string sql = string.Format("Select * from NHANVIEN");
+            if (data.Execute(sql) == false)
+            {
+                return null;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ DataTransferObject/DTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DataTransferObject
+{   
+    public class NhanVien_Object
+    {
+        public int MaNhanVien { get; set; }
+        public string TenNhanVien { get; set; }
+        public string TaiKhoan { get; set; }
+        public string MatKhau { get; set; }
+        public string MaHoa { get; set; }
+    }
+    
+}
